fd40a89c362b9c09666d598467660572b448ae82
Sean Francis N. Ballais <sean@seanballais.com>
2016-08-16 20:11:18 +0800

    Completed the replica of the 2015 design.

--- botos/templates/default/static/js/ee-index.js
+++ botos/templates/default/static/js/ee-index.js
@@ -24,10 +24,15 @@ $(document).ready(function() {
             $('html, body').css({ 'background-image': 'url("/assets/img/ee/bg-2015.jpg")' });
             $('div#banner-2015').show();
             $('img#banner-2016').hide();
-            $('div#base-banner').css({ 'margin-bottom': '3%', 'margin-top': '2%', 'text-align': 'center', 'color': '#ffffff' });
+            $('div#base-banner').css({ 'margin-bottom': '2%', 'margin-top': '1%', 'text-align': 'center', 'color': '#ffffff' });
             $('div#base-banner').removeClass();
             $('div#base-banner h1').addClass('design-2015');
             $('span#banner-title-2015').show();
+            $('form#login-form-2015').show();
+            $('div#footer-2015').delay(2000).show();
+            $('div#fork-me-ribbon').show();
+            $('form#login-form').hide();
+            $('div#footer-2016').hide();
             $('span#design-2015').hide();
         }, 1000);
 
